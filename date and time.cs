d96fe37b3deac38af96065392a585c08e4ef68ac
204-bhanuteja <105474392+204-bhanuteja@users.noreply.github.com>
2022-08-22 22:14:33 +0530

    Add files via upload

---
+++ date and time.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Current Date and Time is : ");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+        }
+    }
+}
